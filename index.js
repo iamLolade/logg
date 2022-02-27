1ecb45ad7a3dc8d637999bc5f6a9884331a870c6
iamLolade <adeniyiotemade@gmail.com>
2022-02-27 23:58:53 +0100

    Created render function

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-let resourse = []
+let resourses = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
 const tabBtn = document.getElementById("tab-btn")
+
+const render = (resourse) => {
+    let listItems = ""
+    for (let i = 0; i < resourse.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${resourse[i]}'>
+                    ${resourse[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
