41e40981e13d86974ded7ef141e5f8f27f4f8bec
iamLolade <adeniyiotemade@gmail.com>
2022-02-27 23:56:43 +0100

    Updated js file, created variables for html elements

---
+++ index.js
@@ -0,0 +1,6 @@
+let resourse = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
