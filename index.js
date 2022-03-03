da7ab73eca53eca96170d6ba345bf195cbca529e
iamLolade <adeniyiotemade@gmail.com>
2022-03-03 03:56:16 +0100

    Updated index.js file

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-let resourses = []
+let resources = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
 const tabBtn = document.getElementById("tab-btn")
+const bin = document.getElementById("trash")
 const resourceFromLocalStorage = JSON.parse( localStorage.getItem("resources") )
 
 const render = (resourse) => {
     let listItems = ""
     for (let i = 0; i < resourse.length; i++) {
         listItems += `
-            <li>
+            <li id=${i}>
                 <a target='_blank' href='${resourse[i]}'>
                     ${resourse[i]}
                 </a>
-                <button class="trash"><img src="./bin.png" alt="trash"/></button>
+                <button onclick="deleteSingle()" class="trash" id=${i}><img src="./bin.png" alt="trash"/></button>
             </li>
         `
     }
@@ -22,28 +23,61 @@ const render = (resourse) => {
 }
 
 if (resourceFromLocalStorage) {
-    resourses = resourceFromLocalStorage
-    render(resourses)
+    resources = resourceFromLocalStorage
+    render(resources)
 }
 
+inputEl.addEventListener("keydown", (e) => {
+    if(e.key === "Enter") {
+        resources.push(inputEl.value)
+        inputEl.value = ""
+        localStorage.setItem("resources", JSON.stringify(resources) )
+        render(resources)
+    }
+})
+
 inputBtn.addEventListener("click", function() {
-    resourses.push(inputEl.value)
+    resources.push(inputEl.value)
     inputEl.value = ""
-    localStorage.setItem("resources", JSON.stringify(resourses) )
-    render(resourses)
+    localStorage.setItem("resources", JSON.stringify(resources) )
+    render(resources)
 })
 
 tabBtn.addEventListener("click", function(){    
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        resourses.push(tabs[0].url)
-        localStorage.setItem("resources", JSON.stringify(resourses) )
-        render(resourses)
+        resources.push(tabs[0].url)
+        localStorage.setItem("resources", JSON.stringify(resources) )
+        render(resources)
     })
 })
 
 deleteBtn.addEventListener("dblclick", function() {
     localStorage.clear()
-    resourses = []
-    render(resourses)
+    resources = []
+    render(resources)
 })
+
+function deleteSingle() {
+    let result;
+    for(let i = 0; i < resources.length; i++) {
+        if(resources.indexOf(resources[i]) !== i) {
+            result = resources
+        } else {
+            resources.splice(i, 1)
+            result = [...resources];
+        }
+    }
+    // let result = resources.filter((val, i, arr) => {
+    //     if(i !== i) {
+    //         return val;
+    //     }
+    // })
+    console.log(result);
+    localStorage.setItem("resources", JSON.stringify(result) ) 
+    location.reload()
+    //render(resources);
+}
+
+ 
+
 
